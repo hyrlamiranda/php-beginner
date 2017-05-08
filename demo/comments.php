0124d418291745667fd58f53b75b05836f490867
hyrlamiranda <hyrlammiranda@gmail.com>
2017-05-07 22:54:47 -0300

    Inserting Comments and practice section one

--- demo/comments.php
+++ demo/comments.php
@@ -5,6 +5,15 @@
 	<title>Document</title>
 </head>
 <body>
+<?php
 
+/* This function below displays text
+
+This is multiline comment */
+
+// one line comment
+
+echo "Hello";
+?>
 </body>
 </html>
