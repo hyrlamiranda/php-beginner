28145265c2bd526f10b4a8e5238ad03599e647b2
hyrlamiranda <hyrlammiranda@gmail.com>
2017-05-08 23:42:44 -0300

    simple math

---
+++ demo/section-two/math.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+<?php
+
+echo 45+34*45/421-45;
+echo "<br>";
+echo 5+4/10;
+echo "<br>";
+echo 56+23;
+echo "<br>";
+echo 543-32;
+echo "<br>";
+echo 34*234;
+echo "<br>";
+echo 12/3;
+
+$number1 = 14;
+$number2 = 23;
+
+echo $number1*$number2;
+
+?>
+
+</body>
+</html>
