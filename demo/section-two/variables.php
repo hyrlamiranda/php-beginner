f5edf8d6026cf034d1ba787739cc39b59121f610
hyrlamiranda <hyrlammiranda@gmail.com>
2017-05-08 23:27:19 -0300

    Creating variables

---
+++ demo/section-two/variables.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+<?php
+
+$name        = 'Hyrla';
+$number      = 100;
+$Number_List = 100;
+
+$NUMBER = 100;
+$NUMBer = 100;
+
+echo $name." ".$NUMBer;
+
+$name = "<h1> Hyrla beautiful </h1>";
+
+?>
+</body>
+</html>
