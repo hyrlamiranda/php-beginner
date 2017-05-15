9a423e6fe73a5fd08df7d35884b36d8beacea45d
hyrlamiranda <hyrlammiranda@gmail.com>
2017-05-14 21:33:54 -0300

    Practice 2

---
+++ demo/section-two/2.php
@@ -0,0 +1,47 @@
+<?php include "functions.php"?>
+<?php include "includes/header.php"?>
+<section class="content">
+
+		<aside class="col-xs-4">
+
+<?php Navigation();?>
+</aside><!--SIDEBAR-->
+
+
+		<article class="main-content col-xs-8">
+
+
+
+<?php
+
+$number1 = 10;
+$number2 = 20;
+
+echo $number1+$number2."<br>";
+
+$lists = array(10, 21, 45, 54, 32);
+
+$lists_assoc = array("number" => 10);
+
+echo $lists[0]."<br>";
+
+echo $lists_assoc["number"]."<br>";
+
+/* Step 1: Make 2 variables called number1 and number2 and set 1 to value 10 and the other 20:
+
+Step 2: Add the two variables and display the sum with echo:
+
+
+Step3: Make 2 Arrays with the same values, one regular and the other associative
+
+Step4: Make a constant and set it to the value of PHP. and use an echo to print it out
+
+
+
+
+ */
+
+?>
+</article><!--MAIN CONTENT-->
+
+<?php include "includes/footer.php"?>
