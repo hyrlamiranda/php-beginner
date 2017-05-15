1c878687f7ab1c23f869097aff4797175237d7fa
hyrlamiranda <hyrlammiranda@gmail.com>
2017-05-14 21:18:28 -0300

    Associative Arrays

---
+++ demo/section-two/arrays_assoc.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+<?php
+
+$number = array("Maria", "Jose", "Carlos");
+
+print_r($number);
+//echo $numberList[2] . "<br> ;
+echo "<br>";
+
+$names = array("first_name" => 'Hyrla', "Last_name" => 'Miranda');
+
+//print_r($names);
+
+echo $names['first_name']." ".$names['Last_name'];
+
+?>
+</body>
+</html>
+
